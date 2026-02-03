cc04ee9a0428f47977587d6fcefb9d01d298d8a9
Isu World <isuworld22@gmail.com>
2026-02-03 21:44:23 +0530

    added the 7A code file

---
+++ Problems/7A.py
@@ -0,0 +1,17 @@
+board = [input().strip() for _ in range(8)]
+
+row_count = 0
+col_count = 0
+
+for i in range(8):
+   if board[[i]] == "BBBBBBBB":
+      row_count += 1
+
+if row_count == 8:
+   print(8)
+else:
+   for j in range(8):
+      if all(board[i][j] == "B" for i in range(8)):
+         col_count += 1
+
+   print(row_count + col_count)
