7d5512ca4276cbd4badddc5b1e595743ac81a5b1
Isu World <isuworld22@gmail.com>
2026-02-20 22:09:31 +0530

    just modified the 5B file

--- Problems/5B.py
+++ Problems/5B.py
@@ -1,26 +1,30 @@
 import sys
 
+# read all lines (including empty ones)
 lines = sys.stdin.read().splitlines()
 
+# find max width
 width = max(len(line) for line in lines)
 
 border = "*" * (width + 2)
 print(border)
 
-left_turn = True
+left_turn = True  # for alternating extra space
 
 for line in lines:
-   spaces = width - len(line)
-
-   left = spaces // 2
-   right = spaces - left
-
-   if spaces % 2 == 1:
-      if left_turn:
-         right -= 1
-         left += 1
-      left_turn = not left_turn
-
-   print("*" + " " * left + line + " " * right + "*")
+    L = len(line)
+    total = width - L
+    
+    left = total // 2
+    right = total // 2
+    
+    if total % 2 == 1:
+        if left_turn:
+            left += 1
+        else:
+            right += 1
+        left_turn = not left_turn
+    
+    print("*" + " " * left + line + " " * right + "*")
 
 print(border)
