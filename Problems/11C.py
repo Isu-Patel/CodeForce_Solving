5a3662f013e112c1e6503db83d1137a8d6f6ab74
Isu Patel <isuworld22@gmail.com>
2026-04-09 22:32:42 +0530

    Just done the 11C problem, in which i have edited some line and tried to make a correct solution.

--- Problems/11C.py
+++ Problems/11C.py
@@ -1,80 +1,88 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
-    t = int(input())
-    for _ in range(t):
-        n, m = map(int, input().split())
-        g = [input().strip() for _ in range(n)]
-        
-        # prefix sum
-        pref = [[0]*(m+1) for _ in range(n+1)]
-        for i in range(n):
-            for j in range(m):
-                pref[i+1][j+1] = (pref[i][j+1] + pref[i+1][j] 
-                                 - pref[i][j] + (g[i][j] == '1'))
-        
-        def get(x1,y1,x2,y2):
-            return pref[x2+1][y2+1] - pref[x1][y2+1] - pref[x2+1][y1] + pref[x1][y1]
-        
-        ans = 0
+t = int(input())
 
-        for i in range(n):
-            for j in range(m):
-                for k in range(1, min(n,m)):
-                    x2 = i+k
-                    y2 = j+k
-                    if x2>=n or y2>=m:
-                        break
-                    
-                    # check 4 borders
-                    if (get(i,j,i,y2)==k+1 and
-                        get(x2,j,x2,y2)==k+1 and
-                        get(i,j,x2,j)==k+1 and
-                        get(i,y2,x2,y2)==k+1):
-                        
-                        # isolation check
-                        ok = True
-                        for x in range(i-1, x2+2):
-                            for y in range(j-1, y2+2):
-                                if 0<=x<n and 0<=y<m:
-                                    if not (i<=x<=x2 and j<=y<=y2):
-                                        if g[x][y]=='1':
-                                            ok = False
-                        if ok:
-                            ans += 1
-        
-        # -------- Diagonal squares --------
-        for i in range(n):
-            for j in range(m):
-                for k in range(1, min(n,m)):
-                    # center (i,j)
-                    if (i-k<0 or i+k>=n or j-k<0 or j+k>=m):
-                        break
-                    
-                    ok = True
-                    
-                    # check 4 diagonals
-                    for d in range(k+1):
-                        if g[i-d][j-d] != '1': ok=False
-                        if g[i-d][j+d] != '1': ok=False
-                        if g[i+d][j-d] != '1': ok=False
-                        if g[i+d][j+d] != '1': ok=False
-                    
-                    if not ok:
-                        continue
-                    
-                    # isolation
-                    for x in range(i-k-1, i+k+2):
-                        for y in range(j-k-1, j+k+2):
-                            if 0<=x<n and 0<=y<m:
-                                if abs(x-i)+abs(y-j) > k:
-                                    if g[x][y]=='1':
-                                        ok=False
-                    
-                    if ok:
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = [input().strip() for _ in range(n)]
+
+    # Prefix sum
+    ps = [[0]*(m+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            ps[i][j] = (a[i-1][j-1] == '1') + ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1]
+
+    def get(x1, y1, x2, y2):
+        x1 = max(x1, 1)
+        y1 = max(y1, 1)
+        x2 = min(x2, n)
+        y2 = min(y2, m)
+        if x1 > x2 or y1 > y2:
+            return 0
+        return ps[x2][y2] - ps[x1-1][y2] - ps[x2][y1-1] + ps[x1-1][y1-1]
+
+    ans = 0
+
+    #Axis-aligned squares
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            k = 2
+            while i + k - 1 <= n and j + k - 1 <= m:
+                x1, y1 = i, j
+                x2, y2 = i + k - 1, j + k - 1
+
+                border = (
+                    get(x1, y1, x1, y2) +
+                    get(x2, y1, x2, y2) +
+                    get(x1+1, y1, x2-1, y1) +
+                    get(x1+1, y2, x2-1, y2)
+                )
+
+                if border == 4*k - 4:
+                    outer = get(x1-1, y1-1, x2+1, y2+1)
+                    inner = get(x1, y1, x2, y2)
+
+                    if outer == inner:
                         ans += 1
-        
-        print(ans)
 
-solve()
+                k += 1
+
+    # Diagonal (diamond) squares
+    for i in range(n):
+        for j in range(m):
+            d = 1
+            while True:
+                x1, x2 = i - d, i + d
+                y1, y2 = j - d, j + d
+
+                if x1 < 0 or x2 >= n or y1 < 0 or y2 >= m:
+                    break
+
+                ok = True
+
+                # check border
+                for k in range(d+1):
+                    if a[i-k][j-(d-k)] != '1': ok = False
+                    if a[i-k][j+(d-k)] != '1': ok = False
+                    if a[i+k][j-(d-k)] != '1': ok = False
+                    if a[i+k][j+(d-k)] != '1': ok = False
+
+                if not ok:
+                    d += 1
+                    continue
+
+                # check no touching outside
+                for x in range(x1-1, x2+2):
+                    for y in range(y1-1, y2+2):
+                        if 0 <= x < n and 0 <= y < m:
+                            if abs(x - i) + abs(y - j) > d and a[x][y] == '1':
+                                ok = False
+
+                if ok:
+                    ans += 1
+
+                d += 1
+
+    print(ans)
