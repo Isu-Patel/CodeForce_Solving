563aec8f5486d865d6962f57b1020b6c5e764eae
Isu Patel <isuworld22@gmail.com>
2026-05-09 20:58:07 +0530

    Updated last code and made the correct code.

--- Problems/23B.py
+++ Problems/23B.py
@@ -1,16 +1,8 @@
 import sys
 
-input = sys.stdin.read
-data = input().split()
+input = sys.stdin.readline
 
-t = int(data[0])
-index = 1
+t = int(input())
 for _ in range(t):
-    n = int(data[index])
-    index += 1
-    if n == 1:
-        print(1)
-    elif n == 2:
-        print(0)
-    else:
-        print(n - 2)
+    n = int(input())
+    print(max(0, n - 2))
