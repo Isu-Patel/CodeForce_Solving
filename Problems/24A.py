9e9f478dc63f7a85462d70adb796e15b20e99aa6
Isu Patel <isuworld22@gmail.com>
2026-05-11 21:12:02 +0530

    Wrote the correct code for 24A.py in the second try.

--- Problems/24A.py
+++ Problems/24A.py
@@ -1,27 +1,60 @@
 import sys
+input = sys.stdin.readline
 
-data = sys.stdin.read().split()
-n = int(data[0])
-edges = {}
-costs = {}
+n = int(input())
 
-for i in range(n):
-    a = int(data[1 + 3 * i])
-    b = int(data[2 + 3 * i])
-    c = int(data[3 + 3 * i])
-    edges[a] = b
-    costs[(a, b)] = c
+# graph[u] = [(v, cost_if_reverse)]
+graph = [[] for _ in range(n + 1)]
 
-sum_clocks = 0
-for i in range(1, n + 1):
-    next_i = i % n + 1
-    if edges[i] != next_i:
-        sum_clock += costs[(i, edges[i])]
+# store reverse costs
+cost = {}
 
-sum_counter = 0
-for i in range(1, n + 1):
-    prev_i = (i - 2) % n + 1
-    if edges[i] != prev_i:
-        sum_counter += costs[(i, edges[i])]
+for _ in range(n):
+    a, b, c = map(int, input().split())
 
-print(min(sum_clock, sum_counter))
+    graph[a].append(b)
+    graph[b].append(a)
+
+    # original direction a -> b
+    # reversing it costs c
+    cost[(a, b)] = 0
+    cost[(b, a)] = c
+
+# reconstruct the cycle order
+cycle = [1]
+prev = -1
+cur = 1
+
+while True:
+    nxts = graph[cur]
+
+    nxt = nxts[0] if nxts[0] != prev else nxts[1]
+
+    if nxt == 1:
+        break
+
+    cycle.append(nxt)
+    prev, cur = cur, nxt
+
+# add start again for easy traversal
+cycle.append(1)
+
+# cost in this direction
+cost1 = 0
+
+for i in range(len(cycle) - 1):
+    u = cycle[i]
+    v = cycle[i + 1]
+
+    cost1 += cost[(u, v)]
+
+# opposite direction
+total_reverse = 0
+
+for (u, v), c in cost.items():
+    if c > 0:
+        total_reverse += c
+
+cost2 = total_reverse - cost1
+
+print(min(cost1, cost2))
