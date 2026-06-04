0bc113272cac0393ba00a9b0175314edc5e9daa3
Isu Patel <isuworld22@gmail.com>
2026-06-04 21:02:15 +0530

    Tried to solved 32B but failed.

--- Problems/32B.py
+++ Problems/32B.py
@@ -1,43 +1,17 @@
 s = input().strip()
 
-n = len(s)
-
-res = []
+ans = 0
 i = 0
 
-while i < n:
-    # find '@'
-    at = s.find('@', i)
+while i < len(s):
+    if s[i] == '.':
+        ans.append('0')
+        i += 1
+    else:
+        if s[i + 1] == '.':
+            ans.append('1')
+        else:
+            ans.append('2')
+        i += 2
 
-    if at == -1:
-        print("No solution")
-        exit()
-
-    # must have at least one char before @
-    if at == i:
-        print("No solution")
-        exit()
-
-    # last email
-    if s.find('@', at + 1) == -1:
-        # must have at least one char after @
-        if at == n - 1:
-            print("No solution")
-            exit()
-
-        res.append(s[i:])
-        break
-
-    # next email exists
-    # need at least one char after @
-    if at + 1 >= n:
-        print("No solution")
-        exit()
-
-    # take one char after @
-    res.append(s[i:at + 2])
-
-    # next email starts there
-    i = at + 2
-
-print(",".join(res))
+print(''.join(ans))
