d66e69d3584c4429c19fb1ef920be3d95963651a
Isu Patel <isuworld22@gmail.com>
2026-08-21 13:42:52 +0530

    Got Accepted Code for 51A on Second Try.

--- Problems/51A.py
+++ Problems/51A.py
@@ -1,3 +1,7 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
 
 shapes = set()
@@ -6,6 +10,7 @@ for _ in range(n):
     row1 = input().strip()
     row2 = input().strip()
 
+    # Skip **
     input()
 
     a = row1[0]
@@ -13,13 +18,11 @@ for _ in range(n):
     c = row2[0]
     d = row2[1]
 
-    rotations = [
-        a + b + c + d,
-        c + a + d + b,
-        d + c + b + a,
-        b + d + a + c
-    ]
+    r1 = a + b + c + d
+    r2 = c + a + d + b
+    r3 = d + c + b + a
+    r4 = b + d + a + c
 
-    shapes.add(min(rotations))
+    shapes.add(min(r1, r2, r3, r4))
 
 print(len(shapes))
