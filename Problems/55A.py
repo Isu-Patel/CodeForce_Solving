67135e3d04c96cdb083ee57478b802b397abd6c0
Isu Patel <isuworld22@gmail.com>
2026-09-17 20:46:25 +0530

    Got Accepted Code for 55A on Third Try Finally.

--- Problems/55A.py
+++ Problems/55A.py
@@ -1,6 +1,13 @@
 n = int(input())
 
-if n == 1 or n % 2 == 0:
+visited = set()
+pos = 0
+
+for k in range(1, 2 * n + 1):
+    pos = (pos + k) % n
+    visited.add(pos)
+
+if len(visited) == n:
     print("YES")
 else:
     print("NO")
