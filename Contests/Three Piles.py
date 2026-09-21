2ef8aca96797e539e68e0fe16b0f918738954f53
Isu Patel <isuworld22@gmail.com>
2026-09-21 22:19:31 +0530

    Tried but got wrong answer on test 1 for 2nd problem.

--- Contests/Three Piles.py
+++ Contests/Three Piles.py
@@ -1,2 +1,15 @@
 # Hi myslef Isu Patel. I post asnwers of Coding Contest for Codeforecs.
 # You can check out my Github Profile.
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+
+    d = abs(a - b)
+
+    print(max(d, c - d))
